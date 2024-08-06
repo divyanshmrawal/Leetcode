899c0796a79b68d937443e43d3b0d94d19064f65
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-08-06 18:17:42 +0530

    Create Add_Two_Integers__3.java
    Add Two Integers

---
+++ Java/Add_Two_Integers__3.java
@@ -0,0 +1,12 @@
+//QUESTION-->
+//Add Two Integers
+
+//ANSWER-->
+class Add_Two_Integers__3 {
+    public int sum(int num1, int num2) {
+        //Addition of 2 numbers
+        int result=num1+num2;
+        //Returning sum of 2 numbers
+        return result;
+    }
+}
