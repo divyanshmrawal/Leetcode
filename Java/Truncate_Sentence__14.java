a38d0dcbb56eb284c61d0fde74f33ab316a15f11
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-08-21 20:00:58 +0530

    Create Truncate_Sentence__14.java

---
+++ Java/Truncate_Sentence__14.java
@@ -0,0 +1,21 @@
+//QUESTION-->
+//A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+
+// For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+// You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
+
+//ANSWER-->
+class Solution {
+    public String truncateSentence(String s, int k) {
+        
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==' '){
+               k--;
+            }
+            if(k==0){
+                return s.substring(0,i);
+            }
+        }
+        return s;
+    }
+}
