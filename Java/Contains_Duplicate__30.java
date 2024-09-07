6ae6d771a5bffc409ead391b46bf8ed005542a73
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-09-07 10:24:06 +0530

    Create Contains_Duplicate__30.java

---
+++ Java/Contains_Duplicate__30.java
@@ -0,0 +1,16 @@
+//QUESTION-->
+//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+//ANSWER-->
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j])
+                    return true;
+            }
+        }
+        return false;
+    }
+}
