4a652e7c2b9693ebe121ad9f15a372502756df24
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-09-10 12:31:01 +0530

    Create Range_Sum_Query_Immutable__34.java

---
+++ Java/Range_Sum_Query_Immutable__34.java
@@ -0,0 +1,26 @@
+//QUESTION-->
+//Given an integer array nums, handle multiple queries of the following type:
+
+// Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+// Implement the NumArray class:
+
+// NumArray(int[] nums) Initializes the object with the integer array nums.
+// int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+ 
+
+//ANSWER-->
+class NumArray {
+    int[] nums;
+        public NumArray(int[] nums) {
+            this.nums=nums;
+        }
+        
+        public int sumRange(int left, int right) {
+            int sum=0;
+            for(int i=left;i<=right;i++){
+                sum+=nums[i];
+            }
+            return sum;
+        }
+    }
+    
