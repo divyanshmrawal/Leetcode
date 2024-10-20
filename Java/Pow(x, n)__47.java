ae8dba8b093130c0a990a0c92b7d4eef03a2371c
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-10-20 19:09:14 +0530

    Create Pow(x, n)__47.java

---
+++ Java/Pow(x, n)__47.java
@@ -0,0 +1,12 @@
+//QUESTION-->
+//Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+//ANSWER-->
+class Solution {
+    public double myPow(double x, int n) {
+        return Math.pow(x,n);
+    }
+}
+
+//Time Complexity: O(logn)
+// Space Complexity: O(1)
